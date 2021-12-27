821c6e61cfaae30e8725907a8dc5ad96204807b6
Adeep Krishna Keelar <eippeekeelar@gmail.com>
2021-12-27 15:34:41 +0530

    Program 11

---
+++ Tutorial 2/c-program.c
@@ -0,0 +1,12 @@
+// This is a single line comment 
+// I like single line comments
+/* Sometimes you've got to put more comments
+  going for lines and lines
+  to bug the program and make it feel nuts */
+#include<stdio.h>
+int main(void) {
+// I dont bother returning args[]
+printf("I know I rule!");
+return 0;
+/* Knightfall */
+}
